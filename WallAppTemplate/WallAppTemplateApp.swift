1c9094f55026e901ec8dd24782cff2266f4cccb9
Henry Langmack <henry.langmack@gmail.com>
2024-07-15 00:09:52 -0700

    Initial Commit

---
+++ WallAppTemplate/WallAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallAppTemplateApp.swift
+//  WallAppTemplate
+//
+//  Created by Henry Langmack on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WallAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
